6803ba3d25d2ce39c6a97ec2ca717bb3ab23a12f
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2026-03-07 12:38:34 +0700

    Add seeder and factories for Product and Kategori, generate 1 user, 10 Kategoris, 20 Products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Create 1 User
+        $user = User::factory()->create([
+            'name' => 'First User',
+            'email' => 'user1@example.com',
         ]);
+
+        // Create 20 Products for that user
+        $products = \App\Models\Product::factory(20)->create([
+            'user_id' => $user->id
+        ]);
+
+        // Create 10 Kategoris (we assign them randomly to the created products)
+        \App\Models\Kategori::factory(10)->create(function () use ($products) {
+            return ['product_id' => $products->random()->id];
+        });
     }
 }
